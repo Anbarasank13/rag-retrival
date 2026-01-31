a6e74b0714cfd4de8f2a1b222dd0a469f5cd617d
anbarasan k <132837405+Anbarasank13@users.noreply.github.com>
2026-01-31 12:54:17 +0530

    Fix import for RecursiveCharacterTextSplitter

--- app_hierarchical.py
+++ app_hierarchical.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
